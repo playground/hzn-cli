f8292aa44e2091fc16e7451ae08c175fac9bc667
Jeff Lu <ljeff@us.ibm.com>
2021-12-06 10:41:53 -0500

    add proper messaging

--- build/commands/deploy.js
+++ build/commands/deploy.js
@@ -46,7 +46,6 @@ const handler = (argv) => {
     const skipInitialize = ['buildMMSImage', 'buildServiceImage', 'dockerImageExists', 'uninstallHorizon'];
     const justRun = ['checkConfigState', 'createHznKey', 'getDeviceArch', 'listDeploymentPolicy', 'listNode', 'listNodePattern', 'listObject', 'listPattern', 'listService', 'removeOrg', 'showHznInfo', 'uninstallHorizon', 'updateHznInfo'];
     const promptForUpdate = ['setup', 'test', 'publishService', 'publishPatterrn', 'publishMMSService', 'publishMMSPattern', 'registerAgent', 'publishMMSObject', 'unregisterAgent'];
-    console.log('$$$ ', action, env, configPath, n);
     const proceed = () => {
         if ((0, fs_1.existsSync)(`${hzn_1.utils.getHznConfig()}/.env-hzn.json`)) {
             const hzn = new hzn_1.Hzn(env, configPath, n, objType, objId, obj, p);
@@ -71,64 +70,70 @@ const handler = (argv) => {
             console.log(`${configPath}/.env-hzn.json file not fouund.`);
         }
     };
-    hzn_1.utils.checkDefaultConfig()
-        .subscribe({
-        complete: () => {
-            if (skipInitialize.indexOf(action) >= 0) {
-                proceed();
+    if (action) {
+        console.log('$$$ ', action, env);
+        hzn_1.utils.checkDefaultConfig()
+            .subscribe({
+            complete: () => {
+                if (skipInitialize.indexOf(action) >= 0) {
+                    proceed();
+                }
+                else if (justRun.indexOf(action) >= 0) {
+                    hzn_1.utils.orgCheck(env, true)
+                        .subscribe({
+                        complete: () => proceed(),
+                        error: (err) => {
+                            console.log(err);
+                            process.exit(0);
+                        }
+                    });
+                }
+                else if (promptForUpdate.indexOf(action) < 0 || skip_config_update) {
+                    hzn_1.utils.orgCheck(env, skip_config_update === 'true')
+                        .subscribe({
+                        complete: () => proceed(),
+                        error: (err) => {
+                            console.log(err);
+                            process.exit(0);
+                        }
+                    });
+                }
+                else {
+                    hzn_1.utils.updateEnvFiles(env)
+                        .subscribe({
+                        complete: () => {
+                            proceed();
+                        }, error: (err) => {
+                            console.log(err);
+                            process.exit(0);
+                        }
+                    });
+                }
+            }, error: (err) => {
+                if (skipInitialize.indexOf(action) < 0) {
+                    console.log(err, 'Initialising...');
+                    hzn_1.utils.setupEnvFiles(env)
+                        .subscribe({
+                        complete: () => {
+                            proceed();
+                        }, error: () => process.exit(0)
+                    });
+                }
+                else {
+                    hzn_1.utils.uninstallHorizon()
+                        .subscribe({
+                        complete: () => {
+                            console.log('process completed.');
+                            process.exit(0);
+                        }
+                    });
+                }
             }
-            else if (justRun.indexOf(action) >= 0) {
-                hzn_1.utils.orgCheck(env, true)
-                    .subscribe({
-                    complete: () => proceed(),
-                    error: (err) => {
-                        console.log(err);
-                        process.exit(0);
-                    }
-                });
-            }
-            else if (promptForUpdate.indexOf(action) < 0 || skip_config_update) {
-                hzn_1.utils.orgCheck(env, skip_config_update === 'true')
-                    .subscribe({
-                    complete: () => proceed(),
-                    error: (err) => {
-                        console.log(err);
-                        process.exit(0);
-                    }
-                });
-            }
-            else {
-                hzn_1.utils.updateEnvFiles(env)
-                    .subscribe({
-                    complete: () => {
-                        proceed();
-                    }, error: (err) => {
-                        console.log(err);
-                        process.exit(0);
-                    }
-                });
-            }
-        }, error: (err) => {
-            if (skipInitialize.indexOf(action) < 0) {
-                console.log(err, 'Initialising...');
-                hzn_1.utils.setupEnvFiles(env)
-                    .subscribe({
-                    complete: () => {
-                        proceed();
-                    }, error: () => process.exit(0)
-                });
-            }
-            else {
-                hzn_1.utils.uninstallHorizon()
-                    .subscribe({
-                    complete: () => {
-                        console.log('process completed.');
-                        process.exit(0);
-                    }
-                });
-            }
-        }
-    });
+        });
+    }
+    else {
+        console.log('specify an action you would like to perform, ex: "oh deploy test" or "oh deploy -h" for help');
+    }
 };
 exports.handler = handler;
 //# sourceMappingURL=deploy.js.map
