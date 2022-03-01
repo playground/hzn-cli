bc2ca0298d226c07882ebb6c2cdf6c68217f0292
Jeff Lu <ljeff@us.ibm.com>
2022-02-28 19:18:04 -0500

    revert envVars

--- bin/install.js
+++ bin/install.js
@@ -28,7 +28,7 @@ const template = {
       "MMS_CONTAINER_NAME": "mms-service",
       "MMS_SERVICE_NAME": "mms-service",
       "MMS_SERVICE_VERSION": "1.0.0",
-      "UPDATE_FILE_NAME": "model.zip
+      "UPDATE_FILE_NAME": "model.zip"
     },
     "metaVars": {
     }
