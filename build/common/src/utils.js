c216374418c254dba1ba18161d0cc3ecb9c964c0
Jeff Lu <ljeff@us.ibm.com>
2022-12-16 12:30:09 -0500

    check if hzn is installed, bypass if not)

--- build/common/src/utils.js
+++ build/common/src/utils.js
@@ -90,13 +90,21 @@ class Utils {
                     this.installHznCli(anax, nodeId, css, token)
                         .subscribe({
                         complete: () => {
-                            this.createHznKey(orgId, this.randomString())
+                            this.shell('hzn architecture')
                                 .subscribe({
                                 complete: () => {
-                                    observer.complete();
+                                    this.createHznKey(orgId, this.randomString())
+                                        .subscribe({
+                                        complete: () => {
+                                            observer.complete();
+                                        },
+                                        error: (err) => {
+                                            observer.error(err);
+                                        }
+                                    });
                                 },
                                 error: (err) => {
-                                    observer.error(err);
+                                    observer.complete();
                                 }
                             });
                         },
