7e21e23bf7a34f3af504d903473b0925287e7c19
Jeff Lu <ljeff@us.ibm.com>
2023-01-25 16:54:50 -0500

    remove hardcoded HZN_MGMT_HUB_CERT_PATH

--- src/common/src/interface/hzn-model.ts
+++ src/common/src/interface/hzn-model.ts
@@ -82,7 +82,7 @@ export const HorizonTemplate = {
   "HZN_NODE_ID": "",
   "HZN_AGBOT_URL": "",
   "HZN_SDO_SVC_URL": "",
-  "HZN_MGMT_HUB_CERT_PATH": "/var/agent-install.crt",
+  "HZN_MGMT_HUB_CERT_PATH": "",
   "HZN_AGENT_PORT": ""
 }
 export const HorizonKeyMap = {
