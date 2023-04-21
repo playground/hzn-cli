5bf723d1c51f4d6bf00444321ac25531da835a9d
Jeff Lu <ljeff@us.ibm.com>
2023-04-20 20:57:57 -0400

    undo add autoSetupCliInContainer logic check

--- src/common/src/utils.ts
+++ src/common/src/utils.ts
@@ -422,7 +422,7 @@ export class Utils {
           },
           error: (err) => observer.error(err)
         })  
-      } else if(setup == SetupEnvironment.autoUpdateConfigFiles || setup == SetupEnvironment.autoSetupCliInContainer) {
+      } else if(setup == SetupEnvironment.autoUpdateConfigFiles) {
         let configJson
         this.updateConfig(configFile)
         .subscribe({
