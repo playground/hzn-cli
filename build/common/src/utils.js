21ea1d9920f60e9cc0ee5834f4d777a84241ca88
Jeff Lu <ljeff@us.ibm.com>
2023-07-06 16:00:56 -0400

    fixed HZN_MGMT_HUB_CERT_PATH

--- build/common/src/utils.js
+++ build/common/src/utils.js
@@ -132,7 +132,7 @@ class Utils {
                     if (key == 'HZN_MGMT_HUB_CERT_PATH') {
                         const cert = /[^\/]+$/.exec(pEnv.CONFIG_CERT_PATH);
                         if (cert) {
-                            content += `/var/${key}=${cert[0]}`;
+                            content += `${key}=/var/${cert[0]}`;
                         }
                     }
                     else {
@@ -150,7 +150,7 @@ class Utils {
                     else if (key == 'HZN_MGMT_HUB_CERT_PATH') {
                         const cert = /[^\/]+$/.exec(pEnv.CONFIG_CERT_PATH);
                         if (cert) {
-                            content += `/var/${key}=${cert[0]}`;
+                            content += `${key}=/var/${cert[0]}`;
                         }
                     }
                 }
