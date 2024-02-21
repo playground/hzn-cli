373aa4314af261cb423211210aab30782b3d68c0
Jeff Lu <ljeff@us.ibm.com>
2024-02-21 16:32:55 -0500

    expose deployment

--- build/common/src/hzn.js
+++ build/common/src/hzn.js
@@ -199,7 +199,7 @@ class Hzn {
     }
     exposeDeployment() {
         return this.param.name.length > 0 && this.param.type.length > 0 && this.param.port.length > 0 ?
-            exports.utils.shell(`kubectl create deployment ${this.param.name} --port ${this.param.port} --type ${this.param.type} -n $AGENT_NAMESPACE`) :
+            exports.utils.shell(`kubectl expose deployment ${this.param.name} --port ${this.param.port} --type ${this.param.type} -n $AGENT_NAMESPACE`) :
             (0, rxjs_1.of)('Please specify deploment --name, --port and --type');
     }
     meshNodeList() {
