03096a8dcc482c3acbbe6dc17ee677504b5909cc
Jeff Lu <ljeff@us.ibm.com>
2025-04-24 15:18:13 -0400

    v0.9.5 remove os.platform

--- src/common/src/utils.ts
+++ src/common/src/utils.ts
@@ -994,7 +994,7 @@ export class Utils {
     return this.shell(arg)
   }
   purgeManagementHub(purge: boolean) {
-    if(purge && (os.arch() == 'x64' || process.platform != 'darwin')) {
+    if(purge && (os.arch() == 'x64')) {
       // TODO:  darwin not supported
       const arg = `curl -sSL https://raw.githubusercontent.com/open-horizon/devops/master/mgmt-hub/deploy-mgmt-hub.sh --output deploy-mgmt-hub.sh && chmod +x deploy-mgmt-hub.sh && sudo ./deploy-mgmt-hub.sh -PS && sudo rm -rf /tmp/horizon-all-in-1`
       return this.shell(arg)  
